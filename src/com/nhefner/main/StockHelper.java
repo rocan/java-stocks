9987da33d7bc497c74bfaf12025200a1dd60058f
rocan <rocan@users.noreply.github.com>
2014-06-15 15:08:40 -0700

    Update StockHelper.java
    Add regular expression to handle market cap correctly,ex:2.1B = 2,100,000,000.

--- src/com/nhefner/main/StockHelper.java
+++ src/com/nhefner/main/StockHelper.java
@@ -1,18 +1,26 @@
 package com.nhefner.main;
-
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StockHelper {
-	
 	public StockHelper() {
-		
 	}
-	
 	public double handleDouble(String x) {
 		Double y;
+		Pattern patternofBillion = Pattern.compile("B$");
+		Matcher matchBillion=patternofBillion.matcher(x);
+		Pattern patternofMillion = Pattern.compile("M$");
+		Matcher matchMillion=patternofMillion.matcher(x);
 		if (Pattern.matches("N/A", x)) {  
-			y = 0.00;   
-		} else { 
+			y = 9999.99;   
+		}
+		else if (matchBillion.find()){
+			y=Double.parseDouble(x.substring(0,x.length()-1))*1E9;
+		}
+		else if (matchMillion.find()){
+			y=Double.parseDouble(x.substring(0,x.length()-1))*1E6;
+		}
+		else { 
 			y = Double.parseDouble(x);  
 		}  
 		return y;
@@ -21,7 +29,7 @@ public class StockHelper {
 	public int handleInt(String x) {
 		int y;
 		if (Pattern.matches("N/A", x)) {  
-			y = 0;   
+			y = 9999;   
 		} else { 
 			y = Integer.parseInt(x);  
 		} 
